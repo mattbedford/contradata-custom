4ada677d2467d50e39baecc2c4fb485da46ebaa7
mattbedford <matt.bedford@gmail.com>
2024-07-15 23:09:44 +0200

    moved slider to new plugin

--- functions.php
+++ functions.php
@@ -6,12 +6,9 @@ if ( ! defined( 'ABSPATH' ) ) {
         exit;
 }
 
-include_once plugin_dir_path(__FILE__) . '/custom-functions/block-loader.php';
-include_once plugin_dir_path(__FILE__) . '/custom-functions/load-swiper.php';
+
 include_once plugin_dir_path(__FILE__) . '/custom-functions/woo-catalogue-only.php';
 
-new BlockLoader();
-new Swiper();
 new WooCatalogueOnly();
 
     
